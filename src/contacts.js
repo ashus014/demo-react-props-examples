b684a1af61dad0016cfc968f6b799f87916435e1
ashus014 <ashu.ask.singh@gmail.com>
2022-04-16 13:23:01 +0530

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Ashu Singh",
+    imgURL:
+      "https://castyou-website.sgp1.digitaloceanspaces.com/2019/09/LV4A9843.jpg",
+    phone: "+91-554466756",
+    email: "info@ashu.com"
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
